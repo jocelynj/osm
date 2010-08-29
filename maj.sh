63110bfb7dbc727e16b5008e65a2b301689c0182
Jocelyn Jaubert <jocelyn.jaubert@gmail.com>
2010-08-29 19:46:07 +0200

    Ajout de la maj des rivières

--- maj.sh
+++ maj.sh
@@ -2,6 +2,13 @@
 
 set -e
 
+date
+
 (cd osmosis && ./update-pgsql.sh && psql osm < CreateGeometryForWays-relations.sql) || exit
 
+date
+
 (cd autoroutes && ./maj.sh)
+(cd rivieres && ./maj.sh)
+
+date
