52798862696699d5ecc903eca82a2117b28ee0d3
Jocelyn Jaubert <jocelyn.jaubert@gmail.com>
2011-10-06 19:46:13 +0200

    Move osmosis-actions.sql to osmosis-hourly

--- osmosis-hourly/osmosis-actions.sql
+++ osmosis-hourly/osmosis-actions.sql
@@ -1,17 +1,12 @@
-CREATE OR REPLACE FUNCTION delete(arr bigint[], val bigint) RETURNS bigint[] AS $$
-BEGIN
-  RETURN ARRAY (
-    SELECT * FROM (SELECT UNNEST(arr) AS a) AS b WHERE a != val
-  );
-END;
-$$ LANGUAGE plpgsql;
-
 CREATE OR REPLACE FUNCTION delete_way(deleted_way_id bigint) RETURNS void AS $$
 BEGIN
   DELETE FROM relation_members WHERE member_type = 'W' AND member_id = deleted_way_id;
   DELETE FROM ways WHERE id = deleted_way_id;
   DELETE FROM nodes USING way_nodes WHERE nodes.id = way_nodes.node_id AND
                                           way_nodes.way_id = deleted_way_id;
+  DELETE FROM actions USING way_nodes WHERE actions.id = way_nodes.node_id AND
+                                            actions.data_type = 'N' AND
+                                            way_nodes.way_id = deleted_way_id;
   DELETE FROM way_nodes WHERE way_nodes.way_id = deleted_way_id;
 
   DELETE from actions WHERE data_type = 'W' AND id = deleted_way_id AND action != 'D';
@@ -23,36 +18,37 @@ CREATE OR REPLACE FUNCTION osmosisUpdate_way() RETURNS void AS $$
 DECLARE
   line RECORD;
 BEGIN
+  SET enable_seqscan TO FALSE;
+  RAISE NOTICE 'way1';
   FOR line IN SELECT actions.id AS id
               FROM actions
               JOIN ways ON ways.id = actions.id AND
                            ways.linestring IS NULL
-              WHERE data_type = 'W' AND action != 'D'
+              WHERE data_type = 'W'
   LOOP
---    RAISE NOTICE 'way1 - %', line.id;
     PERFORM delete_way(line.id);
   END LOOP;
 
+  RAISE NOTICE 'way2';
   FOR line IN SELECT actions.id AS id
               FROM actions
               JOIN ways ON ways.id = actions.id AND st_npoints(ways.linestring) = 1
               JOIN bounding_box ON bounding_box.name = 'all' AND
                                    ST_Disjoint(ST_StartPoint(ways.linestring),
                                                bounding_box.geom)
-              WHERE data_type = 'W' AND action != 'D'
+              WHERE data_type = 'W'
   LOOP
---    RAISE NOTICE 'way2 - %', line.id;
     PERFORM delete_way(line.id);
   END LOOP;
 
+  RAISE NOTICE 'way3';
   FOR line IN SELECT actions.id AS id
               FROM actions
               JOIN ways ON ways.id = actions.id AND st_npoints(ways.linestring) > 1
               JOIN bounding_box ON bounding_box.name = 'all' AND
                                    ST_Disjoint(ways.linestring, bounding_box.geom)
-              WHERE data_type = 'W' AND action != 'D'
+              WHERE data_type = 'W'
   LOOP
-    RAISE NOTICE 'way3 - %', line.id;
     PERFORM delete_way(line.id);
   END LOOP;
 END;
@@ -72,18 +68,18 @@ $$ LANGUAGE plpgsql;
 CREATE OR REPLACE FUNCTION osmosisUpdate_node() RETURNS void AS $$
 DECLARE
   line RECORD;
-  way_node RECORD;
-  nodes_list bigint[];
 BEGIN
-  FOR line IN SELECT actions.id AS id
-              FROM actions
-              LEFT JOIN nodes ON nodes.id = actions.id
-              WHERE data_type = 'N' AND action != 'D' AND
-                    nodes.id IS NULL
-  LOOP
-    RAISE NOTICE 'node1 - %', line.id;
-    PERFORM delete_node(line.id);
-  END LOOP;
+  SET enable_seqscan TO FALSE;
+--  FOR line IN SELECT actions.id AS id
+--              FROM actions
+--              INNER JOIN relation_members ON  member_type = 'N' AND member_id = actions.id
+--              LEFT JOIN nodes ON nodes.id = actions.id
+--              WHERE data_type = 'N' AND
+--                    nodes.id IS NULL
+--  LOOP
+--    RAISE NOTICE 'node1 - %', line.id;
+--    PERFORM delete_node(line.id);
+--  END LOOP;
 
   FOR line IN SELECT actions.id AS id
               FROM actions
@@ -91,43 +87,11 @@ BEGIN
               JOIN bounding_box ON bounding_box.name = 'all' AND
                                    ST_Disjoint(nodes.geom, bounding_box.geom)
               LEFT JOIN way_nodes ON way_nodes.node_id = nodes.id
-              WHERE data_type = 'N' AND action != 'D' AND
+              WHERE data_type = 'N' AND
                     way_nodes.way_id IS NULL
   LOOP
-    RAISE NOTICE 'node2 - %', line.id;
+--    RAISE NOTICE 'node2 - %', line.id;
     PERFORM delete_node(line.id);
-  END LOOP;
-END;
-$$ LANGUAGE plpgsql;
-
-
-  FOR line IN SELECT actions.id AS id
-              FROM actions
-              JOIN nodes ON nodes.id = actions.id
-              JOIN bounding_box ON bounding_box.name = 'all' AND
-                                   ST_Disjoint(nodes.geom, bounding_box.geom)
-              LEFT JOIN way_nodes ON way_nodes.node_id = nodes.id
-              WHERE data_type = 'N' AND action != 'D' AND
-                    way_nodes.way_id IS NULL
-  LOOP
-    RAISE NOTICE 'node2 - %', line.id;
-    PERFORM delete_node(line.id);
-  END LOOP
-
-
-    DELETE FROM relation_members WHERE member_type = 'N' AND member_id = line.id;   
-    FOR way_node IN SELECT * FROM way_nodes WHERE node_id = line.id LOOP
-      SELECT delete(nodes, line.id) INTO nodes_list FROM ways WHERE ways.id = way_node.way_id;
-      IF nodes_list IS NULL THEN
-        DELETE FROM ways WHERE id = way_node.way_id;
-      ELSE
-        UPDATE ways SET nodes = nodes_list WHERE id = way_node.way_id;
-      END IF;
-    END LOOP;
-    DELETE FROM way_nodes WHERE node_id = line.id;
-
-    DELETE FROM nodes WHERE id = line.id;
-    DELETE from actions WHERE data_type = 'N' AND id = line.id AND action != 'D';
   END LOOP;
 END;
 $$ LANGUAGE plpgsql;
@@ -149,4 +113,3 @@ insert into actions select * from actions_bak;
 
 SELECT * FROM osmosisUpdate_way();
 SELECT * FROM osmosisUpdate_node();
-
