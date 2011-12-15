91375c5da4324242bc4d031fac9b7b69a8c7e7f5
Jocelyn Jaubert <jocelyn.jaubert@gmail.com>
2011-12-15 23:13:05 +0100

    Add more index on ways tags

--- osmosis-hourly/init.sql
+++ osmosis-hourly/init.sql
@@ -6,3 +6,13 @@ $BODY$
 $BODY$
 LANGUAGE 'SQL' ;
 
+--INSERT INTO bounding_box VALUES
+--  ('all', (SELECT ST_Union(geom)
+--           FROM bounding_box
+--           WHERE name != 'all'));
+
+CREATE INDEX idx_ways_tags_highway ON ways USING btree((tags->'highway')) where tags ? 'highway';
+CREATE INDEX idx_ways_tags_amenity ON ways USING btree((tags->'amenity')) where tags ? 'amenity';
+CREATE INDEX idx_ways_tags_leisure ON ways USING btree((tags->'leisure')) where tags ? 'leisure';
+CREATE INDEX idx_ways_tags_building ON ways USING btree((tags->'building')) where tags ? 'building';
+CREATE INDEX idx_ways_tags_natural ON ways USING btree((tags->'natural')) where tags ? 'natural';
