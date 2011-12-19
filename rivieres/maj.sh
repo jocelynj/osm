a3532901b3a863c5ec0dbe5d143b70d6919733ec
Jocelyn Jaubert <jocelyn.jaubert@gmail.com>
2011-12-19 19:04:55 +0100

    Rewrite first SQL on rivers intersection to improve speed

--- rivieres/maj.sh
+++ rivieres/maj.sh
@@ -8,27 +8,34 @@ $PREFIX psql "$DATABASE" << SQL
 DROP TABLE IF EXISTS rivers_intersections;
 CREATE TABLE rivers_intersections
 AS
-SELECT DISTINCT ON (rt.id, rtt2.id)
-       rt.id AS id1,   rt.tags->'name' AS name1,  wg.id AS way1,
-       rtt2.id AS id2, rtt2.tags->'name' AS name2, wg2.id AS way2
-FROM relations rt
-JOIN relation_members rm ON rm.relation_id = rt.id AND rm.member_type = 'W'
-JOIN ways wg ON wg.id = rm.member_id AND ST_IsValid(wg.linestring) AND
-                St_NPoints(wg.linestring) > 1
-
-JOIN relations rtt2 ON rtt2.tags ? 'type' AND rtt2.tags->'type' = 'waterway' AND
-                       rtt2.id > rt.id
-JOIN relation_members rm2 ON rm2.relation_id = rtt2.id AND rm2.member_type = 'W'
-JOIN ways wg2 ON wg2.id = rm2.member_id AND ST_IsValid(wg2.linestring) AND
-                 St_NPoints(wg2.linestring) > 1 AND
-                 (wg.bbox && wg2.bbox) AND
-                 ST_Intersects(wg.linestring, wg2.linestring)
-
-WHERE rt.tags ? 'type' AND rt.tags->'type' = 'waterway' AND
-      wg.nodes[array_length(wg.nodes, 1)] != wg2.nodes[array_length(wg2.nodes, 1)];
+SELECT
+    r1.id AS id1, r1.tags->'name' AS name1, wn1.way_id AS way1,
+    r2.id AS id2, r2.tags->'name' AS name2, wn2.way_id AS way2
+FROM
+    relations AS r1
+    JOIN relation_members AS rm1 ON
+        r1.id = rm1.relation_id AND
+        rm1.member_type = 'W'
+    JOIN way_nodes AS wn1 ON
+        rm1.member_id = wn1.way_id
+    JOIN way_nodes AS wn2 ON
+        wn1.way_id != wn2.way_id AND
+        wn1.node_id = wn2.node_id
+    JOIN relation_members AS rm2 ON
+        wn2.way_id = rm2.member_id AND
+        rm2.member_type = 'W' AND
+        rm2.relation_id > rm1.relation_id
+    JOIN relations AS r2 ON
+        rm2.relation_id = r2.id
+WHERE
+    r1.tags?'type' AND
+    r1.tags->'type' = 'waterway' AND
+    r2.tags?'type' AND
+    r2.tags->'type' = 'waterway';
 ALTER TABLE rivers_intersections OWNER to osmosis;
+SQL
 
-
+$PREFIX psql "$DATABASE" << SQL
 DROP TABLE IF EXISTS rivers_coastline_intersections;
 CREATE TABLE rivers_coastline_intersections
 AS
@@ -38,10 +45,12 @@ SELECT DISTINCT ON (rt.id)
 FROM relations rt
 JOIN relation_members rm ON rm.relation_id = rt.id AND rm.member_type = 'W' AND
                             rm.member_role != 'waterbank'
-JOIN ways wg ON wg.id = rm.member_id AND ST_IsValid(wg.linestring)
+JOIN ways wg ON wg.id = rm.member_id AND ST_IsValid(wg.linestring) AND
+                St_NPoints(wg.linestring) > 1
 
 JOIN ways wg2 ON wg2.tags ? 'natural' AND wg2.tags->'natural' = 'coastline' AND
                  ST_IsValid(wg2.linestring) AND
+                 St_NPoints(wg2.linestring) > 1 AND
                  (wg.bbox && wg2.bbox) AND
                  ST_Intersects(wg.linestring, wg2.linestring)
 
@@ -50,4 +59,4 @@ ALTER TABLE rivers_coastline_intersections OWNER to osmosis;
 
 SQL
 
-php suivi-affluents.php > suivi-affluents.html
+$PREFIX php suivi-affluents.php > suivi-affluents.html
