1625d8e951f4d6bc4d60d3e111f2ff22f1c1291e
Jocelyn Jaubert <jocelyn.jaubert@gmail.com>
2012-01-31 20:13:13 +0100

    Add interesting indexes in osmosis database

--- osmosis-hourly/init.sql
+++ osmosis-hourly/init.sql
@@ -11,8 +11,17 @@ LANGUAGE 'SQL' ;
 --           FROM bounding_box
 --           WHERE name != 'all'));
 
-CREATE INDEX idx_ways_tags_highway ON ways USING btree((tags->'highway')) where tags ? 'highway';
+CREATE INDEX idx_nodes_tags_power ON nodes USING btree((tags->'power')) where tags ? 'power';
+
+CREATE INDEX idx_ways_tags_admin_level ON ways USING btree((tags->'admin_level')) where tags ? 'admin_level';
 CREATE INDEX idx_ways_tags_amenity ON ways USING btree((tags->'amenity')) where tags ? 'amenity';
-CREATE INDEX idx_ways_tags_leisure ON ways USING btree((tags->'leisure')) where tags ? 'leisure';
 CREATE INDEX idx_ways_tags_building ON ways USING btree((tags->'building')) where tags ? 'building';
+CREATE INDEX idx_ways_tags_highway ON ways USING btree((tags->'highway')) where tags ? 'highway';
+CREATE INDEX idx_ways_tags_landuse ON ways USING btree((tags->'landuse')) where tags ? 'landuse';
+CREATE INDEX idx_ways_tags_leisure ON ways USING btree((tags->'leisure')) where tags ? 'leisure';
 CREATE INDEX idx_ways_tags_natural ON ways USING btree((tags->'natural')) where tags ? 'natural';
+CREATE INDEX idx_ways_tags_power ON ways USING btree((tags->'power')) where tags ? 'power';
+CREATE INDEX idx_ways_tags_railway ON ways USING btree((tags->'railway')) where tags ? 'railway';
+CREATE INDEX idx_ways_tags_waterway ON ways USING btree((tags->'waterway')) where tags ? 'waterway';
+
+CREATE INDEX idx_relations_tags_type ON relations USING btree((tags->'type')) where tags ? 'type';
