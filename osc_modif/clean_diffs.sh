8f4112fc7b0323da8990b8fdf3a65b708cf89e57
Jocelyn Jaubert <jocelyn.jaubert@gmail.com>
2019-07-21 21:02:34 +0200

    clean_diffs: Keep 2 months instead of 3 to reduce disk space

--- osc_modif/clean_diffs.sh
+++ osc_modif/clean_diffs.sh
@@ -11,7 +11,7 @@ minute_dirs=$(find -name "*.poly" | sed "s%.poly%/minute%")
 cd $G_WORKDIR
 
 num=$(cat planet/minute/state.txt | grep sequenceNumber | cut -d= -f2)
-oldest=$(($num - 3*30*24*60))  # keep 3 months of diff
+oldest=$(($num - 2*30*24*60))  # keep 2 months of diff
 oldest_dir=$(($oldest / 1000))
 for d in $(seq 1000 $oldest_dir); do
   compl_dir=$(printf "%03d/%03d" $(($d / 1000)) $(($d % 1000)))
@@ -19,7 +19,7 @@ for d in $(seq 1000 $oldest_dir); do
     echo $compl_dir
     for c in $minute_dirs; do
       if [ -e $c/$compl_dir ]; then
-#        echo $c/$compl_dir
+        echo $c/$compl_dir
         rm -rf $c/$compl_dir
       fi
     done
