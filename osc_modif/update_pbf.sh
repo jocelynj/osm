d30e6304376507ea9f512365fff5b9749d4dd3cc
Jocelyn Jaubert <jocelyn.jaubert@gmail.com>
2020-11-15 14:59:02 +0000

    Replace osmosis by osmium to update extracts
    As osmium is a lot faster than osmosis, and more modern, it should help the
    speed of extracts generation.

--- osc_modif/update_pbf.sh
+++ osc_modif/update_pbf.sh
@@ -2,14 +2,12 @@
 
 POLY=$1
 WORKDIR=/data/work/osmbin/extracts/$POLY
-OSMOSIS=/usr/bin/osmosis
 
 CURDATE="`date +%F-%R`"
 
-LOCKFILE="$WORKDIR/lock-osmosis-maj"
+LOCKFILE="$WORKDIR/lock-osmium-maj"
 LOGFILE="$WORKDIR/replication-${CURDATE}.log"
 ERRFILE="$WORKDIR/replication-${CURDATE}.err"
-CHANGEFILE="$WORKDIR/change-${CURDATE}.osc.gz"
 SOURCE_OSM_FILE="$WORKDIR/$(basename $POLY).osm.pbf"
 TARGET_OSM_FILE="$WORKDIR/tmp/$(basename $POLY).osm.pbf"
 
@@ -26,21 +24,9 @@ exec 2>"$ERRFILE"
 mkdir -p $WORKDIR/tmp
 cd $WORKDIR
 
-echo "*** Get changes from server"
-cp "$WORKDIR/state.txt" "$WORKDIR/state.txt.old"
-$OSMOSIS --read-replication-interval workingDirectory="$WORKDIR" --simplify-change --write-xml-change "$CHANGEFILE"
-if [ $? -ne 0 ]; then
-  cp "$WORKDIR/state.txt.old" "$WORKDIR/state.txt"
-  rm $LOCKFILE
-  exit 1
-fi
-
-ls -l "$CHANGEFILE"
-
 echo "*** Update $SOURCE_OSM_FILE"
-$OSMOSIS --read-xml-change "$CHANGEFILE" --read-pbf "$SOURCE_OSM_FILE" --apply-change --buffer --write-pbf file="$TARGET_OSM_FILE"
+pyosmium-up-to-date -v "$SOURCE_OSM_FILE" -o "$TARGET_OSM_FILE"
 if [ $? -ne 0 ]; then
-  cp "$WORKDIR/state.txt.old" "$WORKDIR/state.txt"
   rm $LOCKFILE
   exit 1
 fi
@@ -55,8 +41,18 @@ rm "${SOURCE_OSM_FILE}.md5"
 ln "$TARGET_OSM_FILE" "$SOURCE_OSM_FILE"
 ln "${TARGET_OSM_FILE}.md5" "${SOURCE_OSM_FILE}.md5"
 
+osmium fileinfo "$SOURCE_OSM_FILE" > tmp/fileinfo
+
+SEQNUM=$(cat tmp/fileinfo | grep osmosis_replication_sequence_number= | cut -d= -f2)
+TIMESTAMP=$(cat tmp/fileinfo | grep osmosis_replication_timestamp= | cut -d= -f2 | sed s/:/\\\\:/g)
+
+cat << EOF > state.txt
+sequenceNumber=$SEQNUM
+timestamp=$TIMESTAMP
+EOF
+
 rm "$TARGET_OSM_FILE"
 rm "${TARGET_OSM_FILE}.md5"
-rm "$CHANGEFILE"
+rm "tmp/fileinfo"
 
 rm $LOCKFILE
